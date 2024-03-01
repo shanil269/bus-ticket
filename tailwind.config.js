3870a8a8f3f647c0b02b0fbe2967467ba6b0e12f
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2024-03-01 09:48:30 +0900

    chore: linking necessary stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                // text_black: '#131318',
+            }
+        },
     },
     plugins: [],
 }
